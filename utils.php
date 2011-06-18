2f61449b48e9f31420f3b67b3aa2f5fd838a05ea
Peter Law <PeterJCLaw@gmail.com>
2011-06-18 19:48:30 +0100

    Add some string utils; startsWith and endsWith.

--- utils.php
+++ utils.php
@@ -4,3 +4,35 @@ function isAssoc($arr)
 {
 	return array_keys($arr) !== range(0, count($arr) - 1);
 }
+
+function startsWith($string, $start)
+{
+	$stringLen = strlen($string);
+	$startLen = strlen($start);
+
+	if ($startLen > $stringLen)
+	{
+		return False;
+	}
+
+	$actualStart = substr($string, 0, $startLen);
+
+	$startsWith = $actualStart == $start;
+	return $startsWith;
+}
+
+function endsWith($string, $end)
+{
+	$stringLen = strlen($string);
+	$endLen = strlen($end);
+
+	if ($endLen > $stringLen)
+	{
+		return False;
+	}
+
+	$actualEnd = substr($string, -1 * $endLen);
+
+	$endsWith = $actualEnd == $end;
+	return $endsWith;
+}
